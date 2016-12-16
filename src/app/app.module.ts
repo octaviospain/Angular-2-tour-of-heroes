4913708ba09fe2ccd8490f25a9087204b89652fe
Octavio Calleya <octaviospain@gmail.com>
2016-12-16 12:37:37 +0100

    Created HeroesComponent and refactored AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-
-import { AppComponent }  from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-      FormsModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
     ],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
 
+export class AppModule {
 
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license
- */
+}
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,21 +5,8 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
-
-    // See the "Take it slow" appendix
-    getHeroesSlowly(): Promise<Hero[]> {
-        return new Promise<Hero[]>(resolve =>
-            setTimeout(resolve, 2000)) // delay 2 seconds
-            .then(() => this.getHeroes());
-    }
 }
-
-
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license
- */
