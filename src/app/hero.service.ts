5386064bcf95fdb2ff05e931063426a3f4b677e1
Octavio Calleya <octaviospain@gmail.com>
2016-12-21 12:14:32 +0100

    Added deletion and creation of heroes. Now when the name of a hero is changed, the user has to click on save to persist the changes.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,8 +9,34 @@ import { Hero } from './hero';
 export class HeroService {
 
     private heroesUrl = 'api/heroes';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) { }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
 
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
